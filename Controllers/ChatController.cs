a61e39402b2234b724700f73147386384102ad6c
Lucas Mendes figueiroa <mendeslucas156@gmail.com>
2022-10-27 17:09:19 -0300

    Initial commit

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,18 @@
+﻿using Chattest.Hubs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chattest
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : Controller
+    {
+        private readonly IHubContext<Chat> _chatHubContext;
+
+        public ChatController(IHubContext<Chat> ofertaHubContext)
+        {
+            _chatHubContext = ofertaHubContext;
+        }
+    }
+}
---
+++ Hubs/Chat.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Chattest.Hubs
+{
+    public class Chat : Hub
+    {
+        public async Task NewMenssage(string nome, string menssage)
+        {
+
+            await Clients.All.SendAsync(" NewMenssage", nome, menssage);
+
+        }
+    }
+}
